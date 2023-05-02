d7996bff6faa95a56cdade3377c164bf5ae02591
Michal Charemza <michal@charemza.name>
2023-05-02 13:44:51 +0100

    feat: more robust database connection pool

--- django_db_geventpool/backends/postgresql_psycopg2/psycopg2_pool.py
+++ django_db_geventpool/backends/postgresql_psycopg2/psycopg2_pool.py
@@ -11,13 +11,9 @@ logger = logging.getLogger('django.geventpool')
 
 try:
     from gevent import queue
-    from gevent.lock import RLock
 except ImportError:
     from eventlet import queue
     from ...utils import nullcontext
-
-    def RLock():
-        return nullcontext
 
 try:
     from psycopg2 import connect, DatabaseError
@@ -44,43 +40,54 @@ class DatabaseConnectionPool(object):
         # when it is being closed, or it is closed outside of here, the item
         # will be removed automatically
         self._conns = weakref.WeakSet()
+        self._conns_in_progress = 0
         self.maxsize = maxsize
         self.pool = queue.Queue(maxsize=max(reuse, 1))
-        self.lock = RLock()
 
-    @property
-    def size(self):
-        with self.lock:
-            return len(self._conns)
+    def none_if_unusable(self, conn):
+        try:
+            self.check_usable(conn)
+            logger.debug("DB connection reused")
+            return conn
+        except DatabaseError:
+            logger.debug("DB connection was closed")
+            return None
 
     def get(self):
-        try:
-            size = self.size 
-            if size >= self.maxsize:
-                logger.error('%s out of %s database connections used', size, self.maxsize)
-            if size >= self.maxsize or self.pool.qsize():
-                conn = self.pool.get()
-            else:
-                conn = self.pool.get_nowait()
+        conn = None
 
+        # Find usable connection in the pool
+        while conn is None:
             try:
-                # check connection is still valid
-                self.check_usable(conn)
-                logger.debug("DB connection reused")
-            except DatabaseError:
-                logger.debug("DB connection was closed, creating a new one")
-                conn = None
-        except queue.Empty:
-            conn = None
-            logger.debug("DB connection queue empty, creating a new one")
+                conn = none_if_unusable(self.pool.get_nowait())
+            except queue.Empty:
+                break
 
+        # If no usable connection in the pool, and we're at max conns, wait for a
+        # usable connection to be put into the pool. To handle the case where conns
+        # are _never_ put into the pool, say from failed connection that surface
+        # an exception to the user, we check on a loop if we should still wait
+        # or fall through to creating a connection
+        while conn is None
+            if len(self._conns) + self._conns_in_progress < self.maxsize:
+                break
+            logger.error('%s out of %s database connections used', len(self._conns) + self._conns_in_progress, self.maxsize)
+            try:
+                conn = none_if_unusable(self.pool.get(timeout=5))
+            except queue.Empty:
+                pass
+
+        # If still no usable connection, make a new one. In the below, the event loop
+        # can only yield at create_connection, so we don't need a lock to avoid race
+        # conditions
         if conn is None:
+            self._conns_in_progress += 1
             try:
+                logger.debug("Creating a new DB connection")
                 conn = self.create_connection()
-            except Exception:
-                raise
-            else:
                 self._conns.add(conn)
+            finally:
+                self._conns_in_progress -= 1
 
         return conn
 
